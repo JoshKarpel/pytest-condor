a41d8f2a8bc649a2be28d49bacef2ac1c3d8d72c
JoshKarpel <karpel@wisc.edu>
2019-11-19 14:24:17 -0600

    add bash version of docker run script

--- dr.cmd
+++ dr.cmd
@@ -2,8 +2,10 @@
 
 SET CONTAINER_TAG=pytest-condor-test
 
-ECHO Building pytest-condor-test testing container...
+ECHO Building pytest-condor-test container...
 
 docker build -t %CONTAINER_TAG% .
+
+ECHO Launching pytest-condor-test container...
 
 docker run -it --rm --mount type=bind,source="%CD%",target=/home/tester/pytest-condor %CONTAINER_TAG% %*
---
+++ dr.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CONTAINER_TAG=pytest-condor-test
+
+set -e
+
+echo "Building pytest-condor-test container..."
+
+docker build --quiet -t ${CONTAINER_TAG} .
+
+echo "Launching pytest-condor-test container..."
+
+docker run -it --rm --mount type=bind,source="%CD%",target=/home/tester/pytest-condor ${CONTAINER_TAG} $@
